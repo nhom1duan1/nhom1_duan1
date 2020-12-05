659bcb5550d90c1289690fca836011be2d7a8e92
khaitrung147 <74177325+khaitrung147@users.noreply.github.com>
2020-12-05 13:48:54 +0700

    êffe

--- admin/view/user/newAccount.php
+++ admin/view/user/newAccount.php
@@ -37,7 +37,6 @@
 			<!-- SDT -->
 			<div class="form-group">
 				<label>Số điện thoại</label><br>
-<<<<<<< HEAD
 				<input class="form-control" pattern="[0][0-9]{9}" required type="tel" name="txtSDT" placeholder="nhập số điện thoại của bạn"><br>
 =======
 				<input class="form-control" pattern="[0][0-9]{3}-[0-9]{3}-[0-9]{3}" required type="tel" name="txtSDT" placeholder="nhập số điện thoại của bạn"><br>
