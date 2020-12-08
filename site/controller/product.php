1d240ae5574d22a6ce9ea8f5ab413c99d706e401
phamtuananhcc <74180128+phamtuananhcc@users.noreply.github.com>
2020-12-08 21:22:04 +0700

    câcca

--- site/controller/product.php
+++ site/controller/product.php
@@ -76,33 +76,6 @@ $action='index';
             include 'site/view/product.php';
 
         break;
-<<<<<<< HEAD
-=======
-        case 'home': 
-              include 'site/view/home.php';  
-        break;
-       case 'quickview':
-             $id= $_GET['id'];
-            $product= showProductbyId($id);
-            include 'site/view/quickview.php';
-       break;
-       case 'brand':
-        include 'site/model/catalogs.php';
-          $total=countProduct();
-            $limit=isset($_POST['select'])?$_POST['select']:9;
-            $current_page=isset($_GET['current_page']) ? $_GET['current_page'] : 1;
-            $start=($current_page-1)*$limit;
-            $brand = getBrand();
-             $catalogs= getAllCatalog();
-          $ma_hang=$_GET['id'];
-          $product= showProductbyBrand_loai($ma_hang,$start,$limit);
-          include 'site/view/productbyId.php';
-       break;
-       case 'quickview':
-        
-       break;
-
->>>>>>> b6ca1d742ac70cde9f786aee17f96cb78a0c4a46
         default:
             # code...
             break;
